0dd9b9e41a3aace455466c4614a7e842a01a9502
exsky <alex19911118@gmail.com>
2015-05-03 00:44:45 +0800

    Create a new index.php and two boxes for login

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$apple = "123";
+
+
+
+
+
+?>
+<html>
+<head>
+	
+</head>
+<body>
+<h1>ACC:</h1>
+<input type="text" name="acc"/><br>
+<h1>PWD:</h1>
+<input type="password" name="pwd"/><br>
+
+</body>
+</html>
